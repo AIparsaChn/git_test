dc46922fff60aae42c88ec2dc05d19cf57cee317
AIparsaChn <chrkhabyparsa@gmail.com>
2025-04-03 21:40:01 +0330

    This is a test

--- tool.py
+++ tool.py
@@ -2,4 +2,7 @@ print("hello world")
 def power(base, power):
     return base ** power
 
-print("Goodbye")
+print("This is a test.")
+print("This is a test.")
+print("This is a test.")
+print("This is a test.")
